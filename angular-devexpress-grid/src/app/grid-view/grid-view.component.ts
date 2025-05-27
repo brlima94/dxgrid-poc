9c29be2ffe7265da1cb897b55261ee643bfd687b
Bruno Lima <13972465+brlima94@users.noreply.github.com>
2025-05-28 00:17:00 +0100

    fix selected value lost

--- angular-devexpress-grid/src/app/grid-view/grid-view.component.ts
+++ angular-devexpress-grid/src/app/grid-view/grid-view.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { DxDataGridModule } from 'devextreme-angular';
+import { EditorPreparingEvent } from 'devextreme/ui/data_grid';
 
 @Component({
   selector: 'app-grid-view',
@@ -12,6 +13,10 @@ export class GridViewComponent {
   dataSource: any[] = [];
   showSecondField = false;
   showThirdField = false;
+  yesNoDataSource = [
+    { text: 'Yes', value: true },
+    { text: 'No', value: false }
+  ];
 
   constructor() {
     // Initialize with some sample data
@@ -20,21 +25,33 @@ export class GridViewComponent {
     ];
   }
 
-  onEditorPreparing(e: any) {
-    if (e.dataField === 'secondField') {
+  onEditorPreparing(e: EditorPreparingEvent) {
+    if (e.dataField === 'firstField') {
+      e.editorOptions.onValueChanged = (args: any) => {
+        this.onFirstFieldChanged(args);
+        // Update the data source to maintain the value
+        if (e.row?.rowIndex !== undefined) {
+          this.dataSource[e.row.rowIndex].firstField = args.value;
+        }
+      };
+    } else if (e.dataField === 'secondField') {
+      e.editorOptions.visible = this.showSecondField;
       e.editorOptions.onValueChanged = (args: any) => {
         this.onSecondFieldChanged(args);
+        // Update the data source to maintain the value
+        if (e.row?.rowIndex !== undefined) {
+          this.dataSource[e.row.rowIndex].secondField = args.value;
+        }
       };
     } else if (e.dataField === 'thirdField') {
+      e.editorOptions.visible = this.showThirdField;
       e.editorOptions.onValueChanged = (args: any) => {
         this.onThirdFieldChanged(args);
+        // Update the data source to maintain the value
+        if (e.row?.rowIndex !== undefined) {
+          this.dataSource[e.row.rowIndex].thirdField = args.value;
+        }
       };
-    }
-  }
-
-  onRowUpdating(e: any) {
-    if (e.newData.firstField !== undefined) {
-      this.onFirstFieldChanged({ value: e.newData.firstField });
     }
   }
 
@@ -43,12 +60,23 @@ export class GridViewComponent {
     this.showSecondField = e.value;
     if (!e.value) {
       this.showThirdField = false;
+      // Reset the values when first field is false
+      this.dataSource.forEach(row => {
+        row.secondField = false;
+        row.thirdField = false;
+      });
     }
   }
 
   onSecondFieldChanged(e: any) {
     console.warn('Second field changed:', e.value);
     this.showThirdField = e.value;
+    if (!e.value) {
+      // Reset the third field value when second field is false
+      this.dataSource.forEach(row => {
+        row.thirdField = false;
+      });
+    }
   }
 
   onThirdFieldChanged(e: any) {
