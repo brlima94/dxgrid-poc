f55200b68c7ca0a4272603c68e403f738cd71b25
Bruno Lima <13972465+brlima94@users.noreply.github.com>
2025-05-27 23:50:54 +0100

    init

---
+++ angular-devexpress-grid/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { GridViewComponent } from './grid-view/grid-view.component';
+
+export const routes: Routes = [
+  { path: '', component: GridViewComponent }
+];
---
+++ angular-devexpress-grid/src/app/grid-view/grid-view.component.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { DxDataGridComponent } from 'devextreme-angular';
+
+@Component({
+  selector: 'app-grid-view',
+  templateUrl: './grid-view.component.html',
+  styleUrls: ['./grid-view.component.scss']
+})
+export class GridViewComponent {
+  dataSource: any[] = [];
+  showSecondField = false;
+  showThirdField = false;
+
+  constructor() {
+    // Initialize with some sample data
+    this.dataSource = [
+      { id: 1, firstField: false, secondField: false, thirdField: false }
+    ];
+  }
+
+  onEditorPreparing(e: any) {
+    if (e.dataField === 'secondField') {
+      e.editorOptions.onValueChanged = (args: any) => {
+        this.onSecondFieldChanged(args);
+      };
+    } else if (e.dataField === 'thirdField') {
+      e.editorOptions.onValueChanged = (args: any) => {
+        this.onThirdFieldChanged(args);
+      };
+    }
+  }
+
+  onRowUpdating(e: any) {
+    if (e.newData.firstField !== undefined) {
+      this.onFirstFieldChanged({ value: e.newData.firstField });
+    }
+  }
+
+  onFirstFieldChanged(e: any) {
+    console.warn('First field changed:', e.value);
+    this.showSecondField = e.value;
+    if (!e.value) {
+      this.showThirdField = false;
+    }
+  }
+
+  onSecondFieldChanged(e: any) {
+    console.warn('Second field changed:', e.value);
+    this.showThirdField = e.value;
+  }
+
+  onThirdFieldChanged(e: any) {
+    console.warn('Third field changed:', e.value);
+  }
+}
